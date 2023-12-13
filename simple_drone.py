eacbd4c696c07d6ec98ae068ebe9c21d3f679c91
konstantin <kkononenko3@gmail.com>
2023-12-13 16:27:16 +0200

    create functionality to arming motors and reaching the specified height

--- simple_drone.py
+++ simple_drone.py
@@ -11,3 +11,30 @@ import time
 from pymavlink import mavutil
 
 vehicle = connect("127.0.0.1:14550", wait_ready=True)
+
+
+def arm_and_takeoff(target_altitude):
+    print("Basic pre-arm checks")
+    while not vehicle.is_armable:
+        time.sleep(1)
+
+    print("Arming motors")
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    while not vehicle.armed:
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(target_altitude)
+
+    while True:
+        print("Altitude: ", vehicle.location.global_relative_frame.alt)
+        if (vehicle.location.global_relative_frame.alt
+                >= target_altitude * 0.95):
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
+arm_and_takeoff(100)
